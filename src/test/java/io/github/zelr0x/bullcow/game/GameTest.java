7056b0e9bfc130b0e45254cd8905f1387e93d9b3
zelr0x <zelr0xdev@gmail.com>
2019-07-04 14:47:51 +0400

    Added tests for game package

---
+++ src/test/java/io/github/zelr0x/bullcow/game/GameTest.java
@@ -0,0 +1,99 @@
+package io.github.zelr0x.bullcow.game;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.stream.IntStream;
+
+public class GameTest {
+    private Game game;
+
+    @Before
+    public void setUp() {
+        final int[] target = new int[] {0, 1, 2, 3};
+        game = new Game(target);
+    }
+
+    @Test
+    public void testCheckGuess3b0c() {
+        final GuessResult guess = game.checkGuess("0125");
+        Assert.assertEquals("-- 3b0c: bulls--", 3, guess.getBulls());
+        Assert.assertEquals("-- 3b0c: cows--", 0, guess.getCows());
+        Assert.assertEquals("Incorrect guess message",
+                "3B0C", guess.toString());
+    }
+
+    @Test
+    public void testCheckGuess0b0c() {
+        final GuessResult guess = game.checkGuess("9876");
+        Assert.assertEquals("-- 0b0c: bulls--", 0, guess.getBulls());
+        Assert.assertEquals("-- 0b0c: cows--", 0, guess.getCows());
+        Assert.assertEquals("Incorrect guess message",
+                "0B0C", guess.toString());
+    }
+
+    @Test
+    public void testCheckGuess0b4c() {
+        final GuessResult guess = game.checkGuess("3210");
+        Assert.assertEquals("-- 0b4c: bulls--", 0, guess.getBulls());
+        Assert.assertEquals("-- 0b4c: cows--", 4, guess.getCows());
+        Assert.assertEquals("Incorrect guess message",
+                "0B4C", guess.toString());
+    }
+
+    @Test
+    public void testCheckGuess2b1c() {
+        final GuessResult guess = game.checkGuess("5120");
+        Assert.assertEquals("-- 2b1c: bulls--", 2, guess.getBulls());
+        Assert.assertEquals("-- 2b1c: cows--", 1, guess.getCows());
+        Assert.assertEquals("Incorrect guess message",
+                "2B1C", guess.toString());
+    }
+
+    @Test
+    public void testCheckGuess2b2c() {
+        final GuessResult guess = game.checkGuess("0321");
+        Assert.assertEquals("-- 2b2c: bulls--", 2, guess.getBulls());
+        Assert.assertEquals("-- 2b2c: cows--", 2, guess.getCows());
+        Assert.assertEquals("Incorrect guess message",
+                "2B2C", guess.toString());
+    }
+
+    @Test
+    public void testCheckGuess4b0c() {
+        final GuessResult guess = game.checkGuess("0123");
+        Assert.assertEquals("-- 4b0c: bulls--", 4, guess.getBulls());
+        Assert.assertEquals("-- 4b0c: cows--", 0, guess.getCows());
+        Assert.assertEquals("Correct guess message",
+                "4B0C (correct guess)", guess.toString());
+    }
+
+    @Test
+    public void testGenerateTarget() {
+        final Game game = new Game();
+        Assert.assertEquals("-- Length of generated target --",
+                4, game.getTargetDigits().length);
+    }
+
+    @Test
+    public void testPrefixZeros() {
+        final GuessResult guess = game.checkGuess("23");
+        Assert.assertEquals("-- 3b0c: bulls--", 3, guess.getBulls());
+        Assert.assertEquals("-- 3b0c: cows--", 0, guess.getCows());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCheckGuessTooLong() {
+        final int length = game.getTargetDigits().length + 1;
+        final GuessResult guess = game.checkGuess(
+                IntStream.iterate(0, i -> 0)
+                        .limit(length)
+                        .toString());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCheckGuessAlphabetical() {
+        final GuessResult guess = game.checkGuess("abcd");
+    }
+}
