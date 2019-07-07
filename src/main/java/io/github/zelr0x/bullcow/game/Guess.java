2d9c40df36ed6a1bda0c99ebc151b415250b0472
zelr0x <zelr0xdev@gmail.com>
2019-07-07 16:53:47 +0400

    Changed guess to only encapsulate guess info and do nothing else

--- src/main/java/io/github/zelr0x/bullcow/game/Guess.java
+++ src/main/java/io/github/zelr0x/bullcow/game/Guess.java
@@ -1,39 +1,30 @@
 package io.github.zelr0x.bullcow.game;
 
-import io.github.zelr0x.bullcow.util.NumberParseException;
-import io.github.zelr0x.bullcow.util.NumericUtils;
-
 import java.io.Serializable;
-import java.util.Optional;
 
+/**
+ * Encapsulates guess.
+ */
 public final class Guess implements Serializable {
     private static final long serialVersionUID = 9938476665L;
 
     private final String guess;
-    private int[] digits;
-    private boolean checked = true;
 
+    /**
+     * Constructs guess from a specified String.
+     * @param guess a string hopefully containing digits of the guess
+     */
     public Guess(final String guess) {
         this.guess = guess.trim();
     }
 
+    /**
+     * Returns String representation of a guess suitable for further
+     * processing. Formatted without knowing of a game target length so
+     * further formatting is required.
+     * @return String representation of a guess
+     */
     public String getGuess() {
         return guess;
-    }
-
-    public boolean isValid() {
-        if (!checked) {
-            checked = true;
-            try {
-                digits = NumericUtils.getDigits(guess);
-            } catch (final NumberParseException e) {
-                digits = null;
-            }
-        }
-        return digits != null;
-    }
-
-    public Optional<int[]> getDigits() {
-        return isValid() ? Optional.of(digits) : Optional.empty();
     }
 }
