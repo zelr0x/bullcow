3f024d91776039710747ee19f3f07a1521ab5739
zelr0x <zelr0xdev@gmail.com>
2019-07-07 16:55:29 +0400

    Added game-specific exceptions

---
+++ src/main/java/io/github/zelr0x/bullcow/game/exception/LongGuessException.java
@@ -0,0 +1,32 @@
+package io.github.zelr0x.bullcow.game.exception;
+
+import io.github.zelr0x.bullcow.util.NumberParseException;
+
+/**
+ * Exception indicating that String contains non-numeric data.
+ */
+public class LongGuessException extends NumberParseException {
+    private static final String DEFAULT_MSG = "Guess is too long.";
+    /**
+     * Constructs an exception with a given message.
+     * @param message a message
+     */
+    public LongGuessException(final String message) {
+        super(message);
+    }
+
+    /**
+     * Constructs an exception with a default message.
+     */
+    public LongGuessException() {
+        this(DEFAULT_MSG);
+    }
+
+    /**
+     * Constructs an exception with a given message.
+     * @param expectedLength expected length of a parsed String
+     */
+    public LongGuessException(final int expectedLength) {
+        this(DEFAULT_MSG + " Expected length: " + expectedLength);
+    }
+}
