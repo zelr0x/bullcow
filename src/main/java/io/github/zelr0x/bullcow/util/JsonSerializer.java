19ef6019a078375a1c368599f1213d08064cfc66
zelr0x <zelr0xdev@gmail.com>
2019-07-09 17:17:53 +0400

    Added json serializer

---
+++ src/main/java/io/github/zelr0x/bullcow/util/JsonSerializer.java
@@ -0,0 +1,44 @@
+package io.github.zelr0x.bullcow.util;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import java.util.List;
+
+/**
+ * Encapsulates JSON serialization.
+ */
+public final class JsonSerializer {
+    /**
+     * Serializes list to JSON object containing it as an array
+     * with a specified name: { "propertyName": [...list] }
+     * @param propertyName the name of a resulting json array
+     * @param list a list to serialize
+     * @return JSON object containing the list as an array
+     */
+    public static String serialize(final String propertyName,
+                            final List<?> list) {
+        final JsonArray jsonArray = Holder.GSON
+                .toJsonTree(list)
+                .getAsJsonArray();
+        final JsonObject jsonObject = new JsonObject();
+        jsonObject.add(propertyName, jsonArray);
+        return jsonObject.toString();
+    }
+
+    /**
+     * Holder singleton for Gson object.
+     */
+    private static final class Holder {
+        private static final Gson GSON = new GsonBuilder().create();
+    }
+
+    /**
+     * Restricts instantiation.
+     */
+    private JsonSerializer() {
+        throw new AssertionError();
+    }
+}
