cff88cddd24ba4eb60d3d1ab743581fb31247d11
zelr0x <zelr0xdev@gmail.com>
2019-07-09 22:07:13 +0400

    Added class with collection-related convenience methods that don't exist in Java 8

---
+++ src/main/java/io/github/zelr0x/bullcow/util/CollectionUtil.java
@@ -0,0 +1,43 @@
+package io.github.zelr0x.bullcow.util;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collector;
+import java.util.stream.Stream;
+
+/**
+ * CollectionUtil contains utility methods related to collections.
+ */
+public final class CollectionUtil {
+    /**
+     * Java 8 substitute for Set.of() added in Java 9
+     * @param items the items that need to be added to the resulting set
+     * @param <T> the type of the set's elements
+     * @return the set of specified items
+     */
+    @SafeVarargs
+    public static <T> Set<T> immutableSetOf(final T... items) {
+        return Stream.of(items)
+                .collect(ImmutableCollector.toImmutableSet());
+    }
+
+    /**
+     * @see <a href="https://stackoverflow.com/a/37406054">https://stackoverflow.com/a/37406054</a>
+     */
+    private final static class ImmutableCollector {
+        private static <T> Collector<T, Set<T>, Set<T>> toImmutableSet() {
+            return Collector.of(HashSet::new, Set::add, (l, r) -> {
+                l.addAll(r);
+                return l;
+            }, Collections::unmodifiableSet);
+        }
+    }
+
+    /**
+     * Restricts instantiation.
+     */
+    private CollectionUtil() {
+        throw new AssertionError();
+    }
+}
